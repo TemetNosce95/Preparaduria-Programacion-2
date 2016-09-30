3cbfe9cc986a8a3fe8054d207f84ed5592e303ec
Temet Nosce <temet.nosce95@protonmail.ch>
2016-09-29 20:58:33 -0400

    Cambios en el .gitignore y agrego el ejemplo de cliente TCP muy sencillo.

---
+++ Ejemplos/ClienteTCP/src/clientetcp/ClienteTCP.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clientetcp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+/**
+ *
+ * @author temetnosce
+ */
+public class ClienteTCP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        Socket sock = new Socket("localhost", 30000);
+        BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+        
+        while(true){
+            System.out.println("Enviele algo = ");
+            String s = buffer.readLine();
+            
+            if(s == "close") break;
+            
+            sock.getOutputStream().write(s.getBytes());
+        }
+        
+        sock.close();
+    }
+    
+}
